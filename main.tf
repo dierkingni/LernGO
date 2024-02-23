c1b5fab3aa9649c608cfec49b1c83c5544f0d9e3
Dierking <dierkingni@media-saturn.com>
2024-02-23 14:37:57 +0100

    terraform fmt

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ provider "google" {
   region  = "europe-west4"
 }
 
-resource "google_storage_bucket" "my-bucket"{
-    name          = "calculator-bucket"
-    location      = "europe-west4"
-    force_destroy = true
-    uniform_bucket_level_access = true
+resource "google_storage_bucket" "my-bucket" {
+  name                        = "calculator-bucket"
+  location                    = "europe-west4"
+  force_destroy               = true
+  uniform_bucket_level_access = true
 
-    
+
 }
