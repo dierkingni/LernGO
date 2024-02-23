93223773bfc15e0c2bba8e3e95ea4755b6b18ab4
Dierking <dierkingni@media-saturn.com>
2024-02-23 14:30:47 +0100

    tf provider block added

--- main.tf
+++ main.tf
@@ -2,14 +2,6 @@ provider "google" {
   project = "mms-clp-playground2402-a-i2ar"
   region  = "europe-west4"
 }
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-    }
-  }
-}
 
 resource "google_storage_bucket" "my-bucket"{
     name          = "calculator-bucket"
