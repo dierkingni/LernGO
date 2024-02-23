14f62773f4b415af8db7fd5ddcca3d4be98a1d35
Dierking <dierkingni@media-saturn.com>
2024-02-23 10:06:21 +0100

    adding missing provider

--- main.tf
+++ main.tf
@@ -2,9 +2,14 @@ locals {
     project_id = "mms-clp-playground2402-a-i2ar"
 }
 
-provider "google" {
-    project = local.project_id
-    region  = "europe-west4"
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.69.1"
+    }
+  }
 }
 
 resource "google_storage_bucket" "my-bucket"{
