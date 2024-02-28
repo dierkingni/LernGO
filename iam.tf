71955da3758d98adba2533becc922147e1c573c2
Dierking <dierkingni@media-saturn.com>
2024-02-28 08:19:17 +0100

    adding roles to my github service account

--- iam.tf
+++ iam.tf
@@ -7,3 +7,18 @@ resource "google_project_iam_binding" "service-account" {
     "serviceAccount:mms-clp-playground2402-a-i2ar@appspot.gserviceaccount.com",
   ]
 }
+
+resource "google_project_iam_binding" "github-service-account" {
+  project = "mms-clp-playground2402-a-i2ar"
+  //role    = "roles/storage.objectCreator"
+  role = [
+    "roles/storage.admin",
+    "roles/resourcemanager.projectIamAdmin",
+    "roles/cloudfunctions.admin",
+    "roles/cloudfunctions.serviceAgent"
+  ]
+
+  members = [
+    "serviceAccount:githubactions@mms-clp-playground2402-a-i2ar.iam.gserviceaccount.com",
+  ]
+}
