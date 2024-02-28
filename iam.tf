91dd0cf2ed5049f6d600503e4e804356cd565e50
Dierking <dierkingni@media-saturn.com>
2024-02-28 08:21:04 +0100

    giving access to github service account

--- iam.tf
+++ iam.tf
@@ -10,14 +10,13 @@ resource "google_project_iam_binding" "service-account" {
 
 resource "google_project_iam_binding" "github-service-account" {
   project = "mms-clp-playground2402-a-i2ar"
-  //role    = "roles/storage.objectCreator"
-  role = [
+  for_each = toset([
     "roles/storage.admin",
     "roles/resourcemanager.projectIamAdmin",
     "roles/cloudfunctions.admin",
     "roles/cloudfunctions.serviceAgent"
-  ]
-
+  ])
+   role = each.key
   members = [
     "serviceAccount:githubactions@mms-clp-playground2402-a-i2ar.iam.gserviceaccount.com",
   ]
