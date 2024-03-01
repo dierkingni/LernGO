08342eb91f6a45b5bd05426cdfa6f9cc9b616136
Dierking <dierkingni@media-saturn.com>
2024-03-01 09:53:03 +0100

    creating an extra svc account for the cloud function

--- iam.tf
+++ iam.tf
@@ -4,12 +4,19 @@ resource "google_service_account" "service_account" {
   create_ignore_already_exists = "true"
 }
 
+resource "google_service_account" "cloudfunctions_service_account" {
+  account_id                   = "cloudfunctions-account"
+  display_name                 = "cloudfunctions-account"
+  create_ignore_already_exists = "true"
+}
+
+
 
 resource "google_project_iam_binding" "service_account" {
   project = "mms-clp-playground2402-a-x11e"
   role    = "roles/storage.objectUser"
   members = [
-    "serviceAccount:mms-clp-playground2402-a-x11e@appspot.gserviceaccount.com",
+    "serviceAccount:cloudfunctions-account@mms-clp-playground2402-a-x11e.iam.gserviceaccount.com",
   ]
 }
 
